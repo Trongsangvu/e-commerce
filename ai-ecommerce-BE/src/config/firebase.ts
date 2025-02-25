e588057db9d89d026735b15bc6db5b0b9bb21ecd
mangtrungzos <sangv906@gmail.com>
2025-02-25 19:32:53 +0700

    ignore firebase json

--- ai-ecommerce-BE/src/config/firebase.ts
+++ ai-ecommerce-BE/src/config/firebase.ts
@@ -1,5 +1,5 @@
 import admin from 'firebase-admin';
-import * as serviceAccount from '../firebase/ai-ecommerce-de56e-firebase-adminsdk-fbsvc-135bb993e8.json';
+import * as serviceAccount from '../firebase/ai-ecommerce-de56e-firebase-adminsdk-fbsvc-df3077cefb.json';
 
 const serviceAccountTyped = serviceAccount as admin.ServiceAccount;
 
