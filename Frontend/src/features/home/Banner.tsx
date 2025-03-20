8d2719b14ec13bdf60a965dbbd73543573fc5f50
mangtrungzos <sangv906@gmail.com>
2025-03-20 11:16:16 +0700

    UI

--- Frontend/src/features/home/Banner.tsx
+++ Frontend/src/features/home/Banner.tsx
@@ -15,13 +15,18 @@ export const Banner:React.FC = () => {
                                     <img className='h-[245.5px]' src={images.banner1} alt="banner1" />
                                 </Link>
                                 <div className='flex flex-col relative py-34 px-38 bottom-[260px]'>
-                                    <span className='capitalize text-[28px] group-hover:text-white font-bold font-[Poppins]'>
+                                    <span className='capitalize text-[28px] text-[#333] group-hover:text-white font-bold font font-[Poppins-bold]'>
                                         women
                                     </span>
-                                    <span className='group-hover:text-white font-normal font-[Poppins] text-sm text-[#555]'>
+                                    <span className='group-hover:text-white font-normal font font-[Poppins-regular] text-sm text-[#555]'>
                                         Spring 2025
                                     </span>
                                 </div>
+                                <span className="absolute bottom-[-20px] left-[25%] -translate-x-1/2 top-[70%] text-xs font-[Poppins-medium] text-white uppercase opacity-0 scale-50 transition-all duration-500 ease-out group-hover:bottom-2 group-hover:text-lg group-hover:opacity-100 group-hover:scale-100">
+                                    Shop now
+                                </span>
+
+                                <span className="absolute bottom-0 left-[25%] -translate-x-1/2 top-[80%] h-[1px] w-[100px] bg-white origin-center scale-x-0 transition-transform duration-500 ease-out group-hover:scale-x-100"></span>
                             </div>
                         </div>
                         <div className='px-15 pb-30'>
@@ -31,13 +36,18 @@ export const Banner:React.FC = () => {
                                         <img className='h-[245.5px]' src={images.banner2} alt="banner1" />
                                     </Link>
                                     <div className='flex flex-col relative py-34 px-38 bottom-[260px]'>
-                                        <span className='capitalize group-hover:text-white text-[28px] font-bold font-[Poppins]'>
+                                        <span className='capitalize group-hover:text-white text-[28px] text-[#333] font-bold font font-[Poppins-bold]'>
                                             men
                                         </span>
-                                        <span className='group-hover:text-white font-normal font-[Poppins] text-sm text-[#555]'>
+                                        <span className='group-hover:text-white font-normal font font-[Poppins-regular] text-sm text-[#555]'>
                                             Spring 2025
                                         </span>
                                     </div>
+                                    <span className="absolute bottom-[-20px] left-[25%] -translate-x-1/2 top-[70%] text-xs font-[Poppins-medium] text-white uppercase opacity-0 scale-50 transition-all duration-500 ease-out group-hover:bottom-2 group-hover:text-lg group-hover:opacity-100 group-hover:scale-100">
+                                    Shop now
+                                </span>
+
+                                <span className="absolute bottom-0 left-[25%] -translate-x-1/2 top-[80%] h-[1px] w-[100px] bg-white origin-center scale-x-0 transition-transform duration-500 ease-out group-hover:scale-x-100"></span>
                                 </div>
                             </div>
                         <div className='px-15 pb-30'>
@@ -47,13 +57,18 @@ export const Banner:React.FC = () => {
                                     <img className='h-[245.5px]' src={images.banner3} alt="banner1" />
                                 </Link>
                                 <div className='flex flex-col relative py-34 px-38 bottom-[260px]'>
-                                    <span className='group-hover:text-white capitalize text-[28px] font-bold font-[Poppins]'>
+                                    <span className='group-hover:text-white capitalize text-[28px] text-[#333] font-bold font font-[Poppins-bold]'>
                                         Accessories
                                     </span>
-                                    <span className='group-hover:text-white font-normal font-[Poppins] text-sm text-[#555]'>
+                                    <span className='group-hover:text-white font-normal font font-[Poppins-regular] text-sm text-[#555]'>
                                         Spring 2025
                                     </span>
                                 </div>
+                                <span className="absolute bottom-[-20px] left-[25%] -translate-x-1/2 top-[70%] text-xs font-[Poppins-medium] text-white uppercase opacity-0 scale-50 transition-all duration-500 ease-out group-hover:bottom-2 group-hover:text-lg group-hover:opacity-100 group-hover:scale-100">
+                                    Shop now
+                                </span>
+
+                                <span className="absolute bottom-0 left-[25%] -translate-x-1/2 top-[80%] h-[1px] w-[100px] bg-white origin-center scale-x-0 transition-transform duration-500 ease-out group-hover:scale-x-100"></span>
                             </div>
                         </div>
                     </div>
--- Frontend/src/features/home/Container.tsx
+++ Frontend/src/features/home/Container.tsx
@@ -17,7 +17,7 @@ const Container:React.FC = () => {
         <main className="w-full">
             <div className="flex flex-col pb-52">
                 <div className="pb-10">
-                    <h3 className="uppercase text-4xl font-bold">product overview</h3>
+                    <h3 className="uppercase text-4xl text-[#333] font font-[Poppins-bold]">product overview</h3>
                 </div>
                 <div className="flex flex-col md:flex-row justify-between items-center ">
                     <div className="flex flex-wrap mb-6 md:mb-0">
@@ -30,10 +30,10 @@ const Container:React.FC = () => {
                                     onClick={() => setActiveTab(item.id)}
                                 >
                                     
-                                    <span className="relative inline-block pb-3">
+                                    <span className="relative inline-block font font-[Poppins-regular] pb-3">
                                         {item.label}
                                         <span 
-                                            className={`absolute -bottom-1 left-0 h-0.5 bg-[#797979] transition-all duration-300 ease-in-out
+                                            className={`absolute -bottom-1 left-0 h-1 bg-[#797979] transition-all duration-300 ease-in-out
                                             ${activeTab === item.id ? 'w-full' : 'w-0'}`}
                                         ></span>
                                     </span>
@@ -48,7 +48,7 @@ const Container:React.FC = () => {
                                 <img className="w-12 h-15 transition-colors filter group-hover:brightness-0 group-hover:invert" 
                                     src={images.filterIcon} 
                                 />
-                                <p className="capitalize pl-6 text-[#888] group-hover:text-white font-[var(--font-pops)]">
+                                <p className="capitalize pl-6 text-[#888] text-[15px] font font-[Poppins-regular] group-hover:text-white">
                                     filter
                                 </p>
                             </div>
@@ -56,7 +56,7 @@ const Container:React.FC = () => {
                                 <img  className="w-17 h-20 transition-colors filter group-hover:brightness-0 group-hover:invert" 
                                     src={images.searchIcon} 
                                 />
-                                <p className="capitalize pl-6 text-[#888] group-hover:text-white font-[var(--font-pops)]">
+                                <p className="capitalize pl-6 text-[#888] text-[15px] font font-[Poppins-regular] group-hover:text-white">
                                     search
                                 </p>
                             </div>
