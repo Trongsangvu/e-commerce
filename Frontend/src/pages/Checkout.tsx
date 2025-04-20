b8386e03f45727e030f733a5ad77d0e742c1502b
mangtrungzos <sangv906@gmail.com>
2025-04-20 14:03:56 +0700

    Checkout - Order

--- Frontend/src/pages/Checkout.tsx
+++ Frontend/src/pages/Checkout.tsx
@@ -1,21 +1,39 @@
-function Checkout() {
+export const Checkout: React.FC = () => {
     return ( 
         <>
             <header className="h-[120px]">
-                <div className="bg-[#1b1b1b] h-[120px] flex">
+                <div className="bg-[#1b1b1b] h-[120px] flex justify-between items-center px-27">
                     <div>
-                        <a className="text-[#fff]" href="">Back to Shopping Bag</a>
+                        <a className="text-[#fff] font-[GucciSansPro-light] tracking-[-0.03rem] text-[13px]" href="">Back to Shopping Bag</a>
                     </div>
                     <div>
-                        <h3 className="text-[#fff]">cozastore</h3>
+                        <h3 className="text-[#fff] font-[GucciSansPro-medium uppercase text-[30px] tracking-[2px]">cozastore</h3>
                     </div>
                     <div>
-                        <span className="text-[#fff]">
-                            +84 393 400 682
+                        <span className="text-[#fff] font-[GucciSansPro-light] text-[13px]">
+                            +84.393.400.682
                         </span>
                     </div>
                 </div>
-            </header>        
+            </header>       
+            <div className="content">
+                <div>
+                    <div>
+                        <div>
+                            <div>
+                                <span className="font-[GucciSansPro-book] tracking-[-0.03rem] uppercase text-[12px] text-[#000]">you are checking out as:</span>
+                                <span className="text-[12px] text-[#000] font-[GucciSansPro-book]"></span>
+                            </div>
+                        </div>
+                        <section>
+
+                        </section>
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+            </div> 
         </>
      );
 }
