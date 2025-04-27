62c3976b6b8e939beabbb4aa76d05302a8890d49
mangtrungzos <sangv906@gmail.com>
2025-04-27 21:55:09 +0700

    'update'

--- Backend/.eslintrc.js
+++ Backend/.eslintrc.js
@@ -1,9 +1,22 @@
 module.exports = {
   root: true,
+  env: {
+    node: true,
+    es2022: true
+  },
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
   plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended'
-  ]
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking'
+  ],
+  rules: {
+    // Thêm rules tùy chỉnh tại đây
+  }
 };
