532bd6074c0b51e798c9ff72c7aa258300e29e8a
Johan - UTer <101387957+Trongsangvu@users.noreply.github.com>
2025-08-07 09:38:14 +0700

    fix: eslint

---
+++ Backend/eslint.config.ts
@@ -0,0 +1,27 @@
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+
+export default [
+  {
+    ignores: ["**/*.d.ts", "dist/**/*"],
+  },
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: "module",
+        project: "./tsconfig.json",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "no-undef": "off",
+    },
+  },
+];
